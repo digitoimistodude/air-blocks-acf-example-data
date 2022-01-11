faab8c453adb5f78bb53c4b65909946fb8ad7783
Timi Wahalahti <timi@wahalahti.fi>
2022-01-11 18:26:49 +0200

    create automated all blocks page with example data

--- create-blocks-page.php
+++ create-blocks-page.php
@@ -6,8 +6,8 @@
  *
  * @Author:		Elias Kautto
  * @Date:   		2022-01-11 15:02:49
- * @Last Modified by:   Elias Kautto
- * @Last Modified time: 2022-01-11 17:13:32
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2022-01-11 18:24:17
  *
  * @package air-blocks
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
@@ -15,20 +15,28 @@
 
 namespace ACF_Blocks_Example_Data;
 
-add_action( 'init', __NAMESPACE__ . '\air_blocks_add_rewrite_rules' );
-function air_blocks_add_rewrite_rules() {
-  add_rewrite_rule( '^blocks', 'index.php?orderby=true', 'top' );
-}
+add_action( 'init', function() {
+  add_rewrite_rule( '^blocks', 'index.php?airallblocks=true', 'top' );
+} );
 
-// add_filter( 'query_vars', __NAMESPACE__ . '\air_blocks_query_vars' );
-function air_blocks_query_vars( $vars ) {
-  $vars[] = 'test';
+add_filter( 'query_vars', function( $vars ) {
+  $vars[] = 'airallblocks';
 
   return $vars;
-}
+} );
 
-add_filters( 'template_include', function( $template ) {
-  var_dump( $template );
+add_filter( 'template_include', function( $template ) {
+  if ( ! get_query_var( 'airallblocks', false ) ) {
+    return $template;
+  }
 
-  return $template;
+  if ( ! function_exists( 'acf_get_block_types' ) ) {
+    return $template;
+  }
+
+  if ( 'production' === wp_get_environment_type() && ! is_user_logged_in() ) {
+    return $template;
+  }
+
+  return plugin_dir_path( __FILE__ ) . '/template-all-blocks.php';
 } );
--- template-all-blocks.php
+++ template-all-blocks.php
@@ -6,11 +6,35 @@
  *
  * @Author:		Elias Kautto
  * @Date:   		2022-01-11 17:03:34
- * @Last Modified by:   Elias Kautto
- * @Last Modified time: 2022-01-11 17:03:38
+ * @Last Modified by:   Timi Wahalahti
+ * @Last Modified time: 2022-01-11 18:21:53
  *
  * @package air-blocks
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  */
 
+$blocks = acf_get_block_types();
 
+get_header(); ?>
+
+<main class="site-main">
+  <div class="air-blocks-list" id="blocks">
+
+    <?php foreach ( $blocks as $block ) {
+      if ( ! isset( $block['example'] ) ) {
+        continue;
+      }
+
+      if ( ! isset( $block['example']['attributes']['data'] ) || empty( $block['example']['attributes']['data'] ) ) {
+        continue;
+      }
+
+      $block['data'] = $block['example']['attributes']['data'];
+
+      echo acf_rendered_block( $block, '', true, 0 );
+    } ?>
+
+  </div>
+</main>
+
+<?php get_footer();
